a61b0b150e4f37a71cd6c10b63b2c199f22956e3
LUCAS_3941 <lucas.araujo.pereira23@gmail.com>
2024-05-07 09:57:53 -0300

    added 3 elements modififed 1 element

---
+++ AulaDS2024-06/src/Figura.java
@@ -0,0 +1,11 @@
+public class Figura {
+    public String cor;
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public String getCor(){
+        return (cor);
+    }
+}
---
+++ AulaDS2024-06/src/Retangulo.java
@@ -0,0 +1,35 @@
+public class Retangulo extends Figura {
+    private double lado1;
+    private double lado2;
+
+    public Retangulo(double lado1, double lado2, String cor){
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.cor = cor;
+    }
+
+    public void setLado1(double lado1){
+        this.lado1 = lado1;
+    }
+
+    public void setLado2(double lado2){
+        this.lado2 = lado2;
+    }
+
+    public double getLado1(){
+        return (lado1);
+    }
+
+    public double getLado2(){
+        return (lado2);
+    }
+
+    public double area(){
+        double area = lado1 * lado2;
+        return area;
+    }
+
+    public String toString(){
+        String  = String.valueOf(area);
+    }
+}
